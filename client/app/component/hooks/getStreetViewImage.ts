79dc5f4a759e20d6bc5c6040612904e4c7156edc
linnsheng <linnsheng2002@gmail.com>
2025-04-10 14:37:29 +0800

    update directory structure

--- client/app/component/hooks/getStreetViewImage.ts
+++
@@ -1,5 +0,0 @@
-const getStreetViewUrl = (latitude: number, longitude: number) => {
-  return `https://maps.googleapis.com/maps/api/streetview?size=600x300&location=${latitude},${longitude}&key=${process.env.EXPO_PUBLIC_GOOGLE_API_KEY}`;
-};
-
-export default getStreetViewUrl;
